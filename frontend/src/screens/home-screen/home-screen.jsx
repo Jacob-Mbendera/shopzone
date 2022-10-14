c5fc791b0f8d3c2fbd0b80ab871b725812a2417d
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-10-14 16:13:44 +0200

    useState() ReactHook to manage state

--- frontend/src/screens/home-screen/home-screen.jsx
+++ frontend/src/screens/home-screen/home-screen.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import data from '../../data/data';
+// import data from '../../data/data'; 
+import axios from 'axios';
 
 const HomeScreen  = ()=>{
+const [products, setProducts ] =useState([]);
 
+useEffect(()=> {
+  const fetchData = async() =>{
+      const res = await axios.get('/api/products');
+      setProducts(res.data);
+  }
+
+  fetchData();
+},[])
 
     return (
         <div> 
             <h1>Featured Products</h1>
           <div className="products">
             {
-            data.products.map((product) => (
+            products.map((product) => (
               <div className="product " key={product.slug}>
                 <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.slug} />
