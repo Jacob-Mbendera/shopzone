2a07b23eb2935daa2f48402898bf0cdc9320e688
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-10-14 14:28:19 +0200

    first commitAndRouting

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/home-screen/home-screen';
+import ProductScreen from './screens/product-screen/product-screen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">ShopZone</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={ <ProductScreen />} /> 
+            <Route path='/' element ={ <HomeScreen />} />
+          </Routes> 
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
