b0e7ba990d6936d9885edce6a3a1f3b0bda89aa6
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-11-02 12:23:45 +0200

    NodeServerLocal

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import express from 'express'; 
+import express from 'express';
 import data from './data/data.js';
 
-const app =  express();
 
-app.get('/api/products', (req,res)=>{
+const app = express();
+
+app.get('/api/products', (req,res) =>{
     res.send(data.products);
-});
+})
 
-const port = process.env.PORT || 5001   ;
+const port = process.env.port || 5001;
 
-
-app.listen(port, ()=>{
-    console.log(`Listening at http://localhost:${port}`);
-});
+app.listen(port, () =>{
+    console.log(`server running at https://localhost:${port}`);
+})
