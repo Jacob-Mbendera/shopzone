178e49c06900b462d172c363569001555c55b37c
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-11-02 16:32:40 +0200

    AddReactBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/home-screen/home-screen';
 import ProductScreen from './screens/product-screen/product-screen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <header>
-          <Link to="/">ShopZone</Link>
+        <header> 
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>ShopZone</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
 
         <main> 
-          <Routes>
-            <Route path='/product/:slug' element={ <ProductScreen />} /> 
-            <Route path='/' element ={ <HomeScreen />} />
-          </Routes> 
+          <Container className='d-flex flex-column site-container'>
+            <Routes>
+              <Route path='/product/:slug' element={ <ProductScreen />} /> 
+              <Route path='/' element ={ <HomeScreen />} />
+            </Routes> 
+          </Container>
         </main>
+
+        <footer>
+          <div className='text-center'> All rigthts reserved </div>
+        </footer>
+
       </div>
     </BrowserRouter>
   );
