f54b10cd4d4f0da85666730dec0374672d9985d7
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-11-04 18:13:47 +0200

    ProductCompont,RatingComponent, StarratingWithFontAwesome

---
+++ frontend/src/components/product/product.component.jsx
@@ -0,0 +1,29 @@
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Rating from '../rating/rating.component';
+
+
+
+const Product = (props) => {
+    const {product} = props;
+    return(
+    <Card>
+        <Link to={`/product/${product.slug}`}>
+        <img src={product.image} alt={product.slug}  className="card-img-top"/>
+        </Link>
+
+        <Card.Body>
+            <Link to={`/product/${product.slug}`}>
+                <Card.Title> {product.name}</Card.Title>
+            </Link>
+            <Rating rating={product.rating} numReviews={product.numReviews} />
+            <Card.Text>{product.price}</Card.Text>
+            <Button>Add to Cart</Button>
+        </Card.Body> 
+    </Card>
+    )
+ 
+}
+
+    export default Product;
--- frontend/src/screens/home-screen/home-screen.jsx
+++ frontend/src/screens/home-screen/home-screen.jsx
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import { useReducer } from "react";
 import logger from 'use-reducer-logger';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../../components/product/product.component";
 
 const reducer = (state, action)=>{
 
@@ -76,24 +78,13 @@ const HomeScreen  = ()=>{
             ) : 
 
             (
-            products.map((product) => (
-              <div className="product " key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.slug} />
-                </Link>
-
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p> {product.name}</p>
-                  </Link>
-
-                  <p>
-                    <strong> {product.price} </strong>
-                  </p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            ))
+            <Row>
+                { products.map((product) => (
+                  <Col key={product.slug} sm={6} md ={4} lg={3} className="mb-3">
+                      <Product product={product} ></Product>
+                    </Col>
+                  ))}
+            </Row>
             
             )}
           </div>
