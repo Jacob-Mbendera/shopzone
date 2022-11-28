933e46173267ea67bdabe55c62ee2c6f89131374
Jacob Mbendera <jaybmbendera96@gmail.com>
2022-11-28 21:56:48 +0200

    Added Mongoose, dotenv packages and ConfiguredMongoDB Connection String and created MongoDb Account Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 import express from 'express';
 import data from './data/data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then( () =>{
+    console.log("Connected to the Database")
+})
+.catch((error) =>{
+    console.log(error.message);
+});
 
 
 const app = express();
