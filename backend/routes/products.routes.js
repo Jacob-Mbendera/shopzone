2cbe7155ff110d2b25dd4db8d843092a8fa01b87
Jacob Mbendera <jaybmbendera96@gmail.com>
2023-02-17 21:46:10 +0200

    and point to fetch categories

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -10,7 +10,11 @@ productRouter.get('/', async (req, res) =>{
     res.send(products);
 });
 
+productRouter.get('/categories', expressAsyncHandler(async(req,res)=>{
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
 
+}))
 
 productRouter.get('/slug/:slug', async(req,res) =>{
     const product = await Product.findOne({slug: req.params.slug});
